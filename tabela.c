7104898b4dc3e9b88e2f17d8c642c27c16eee6b5
joaopedroinfj11 <j40444018@gmail.com>
2025-07-17 12:04:25 -0300

    Create tabela.c

---
+++ tabela.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    
+printf("Total     =      100%");
+printf("IVA       =       17%");
+printf("IRS       =       15%");
+printf("---------------------");
+printf("Liq       =       68%");
+
+
+    return 0;
+}
